5208ea35147cf7d45357e76887de2096280f3fd4
FilthyS <xiechengyu1110@gmail.com>
2026-03-30 01:06:31 -0400

    add: quote random tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -35,3 +35,34 @@ def test_quote_by_emotion_scenarios_return_string():
 def test_quote_by_emotion_invalid_emotion_raises():
     with pytest.raises(ValueError, match="Emotion not found"):
         core.QuoteByEmotion("unknown_emotion")
+
+# QuoteRandom tests
+
+# test random deterministically is a bit harder,
+# here the monkeypatch from pytest is used
+
+def test_quote_random_return_string_from_dataset(monkeypatch):
+    test_data = [{"text": "foo"}, {"text": "bar"}]
+    monkeypatch.setattr(core, "quotes", test_data)
+    monkeypatch.setattr(core.random, "choice", lambda seq: seq[1])
+
+    result = core.QuoteRandom()
+    assert isinstance(result, str)
+    assert result == "bar"
+
+def test_quote_random_empty_quote_raises_value_error(monkeypatch):
+    monkeypatch.setattr(core, "quotes", [])
+    with pytest.raises(ValueError, match="quote list is empty"):
+        core.QuoteRandom()
+
+def test_quote_random_none_text_raise_value_error(monkeypatch):
+    monkeypatch.setattr(core, "quotes", [{"text": None}])
+    monkeypatch.setattr(core.random, "choice", lambda seq: seq[0])
+    with pytest.raises(ValueError, match="invalid quote object"):
+        core.QuoteRandom()
+
+def test_quote_random_non_string_text_raise_value_error(monkeypatch):
+    monkeypatch.setattr(core, "quotes", [{"text": 123}])
+    monkeypatch.setattr(core.random, "choice", lambda seq: seq[0])
+    with pytest.raises(ValueError, match="invalid quote text"):
+        core.QuoteRandom()
